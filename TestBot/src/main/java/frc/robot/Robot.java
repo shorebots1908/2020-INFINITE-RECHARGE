9dd517543478a48ef5b04eefb9680d1293984b34
shorebots1908 <60295290+shorebots1908@users.noreply.github.com>
2020-01-25 11:11:47 -0500

    Initial Commit

---
+++ TestBot/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,77 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PWMVictorSPX;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.DemandType;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+
+/**
+ * This is a demo program showing the use of the DifferentialDrive class.
+ * Runs the motors with arcade steering.
+ */
+public class Robot extends TimedRobot {
+  private final TalonSRX _leftMaster = new TalonSRX(1);
+  private final TalonSRX _rightMaster = new TalonSRX(2);
+  private final Joystick _gamepad = new Joystick(0);
+
+  @Override
+	public void teleopInit(){
+		/* Ensure motor output is neutral during init */
+		_leftMaster.set(ControlMode.PercentOutput, 0);
+		_rightMaster.set(ControlMode.PercentOutput, 0);
+
+		/* Factory Default all hardware to prevent unexpected behaviour */
+		_leftMaster.configFactoryDefault();
+		_rightMaster.configFactoryDefault();
+		
+		/* Set Neutral mode */
+		_leftMaster.setNeutralMode(NeutralMode.Brake);
+		_rightMaster.setNeutralMode(NeutralMode.Brake);
+		
+		/* Configure output direction */
+		_leftMaster.setInverted(false);
+		_rightMaster.setInverted(true);
+		
+		System.out.println("This is Arcade Drive using Arbitrary Feed Forward.");
+	}
+
+  @Override
+	public void teleopPeriodic() {		
+		/* Gamepad processing */
+		double forward = -1 * _gamepad.getY();
+		double turn = _gamepad.getTwist();		
+		forward = Deadband(forward);
+		turn = Deadband(turn);
+
+		/* Arcade Drive using PercentOutput along with Arbitrary Feed Forward supplied by turn */
+		_leftMaster.set(ControlMode.PercentOutput, forward, DemandType.ArbitraryFeedForward, +turn);
+		_rightMaster.set(ControlMode.PercentOutput, forward, DemandType.ArbitraryFeedForward, -turn);
+	}
+
+	/** Deadband 5 percent, used on the gamepad */
+	double Deadband(double value) {
+		/* Upper deadband */
+		if (value >= +0.05) 
+			return value;
+		
+		/* Lower deadband */
+		if (value <= -0.05)
+			return value;
+		
+		/* Outside deadband */
+		return 0;
+	}
+}
