ae6b57548474817c4188713174753ed3fae45960
shorebots1908 <60295290+shorebots1908@users.noreply.github.com>
2020-01-25 13:59:31 -0500

    File Moved Errors fixed

--- TestBot/src/main/java/frc/robot/Robot.java
+++ TestBot/src/main/java/frc/robot/Robot.java
@@ -9,17 +9,17 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+/* import edu.wpi.first.wpilibj.drive.DifferentialDrive; */
 
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.DemandType;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+/* import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
-import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.GenericHID.Hand; */
 import edu.wpi.first.networktables.*;
 
 
@@ -109,43 +109,44 @@ public class Robot extends TimedRobot {
 		/* Outside deadband */
 		return 0;
 	}
-}
 
-public void Update_Limelight_Tracking()
-  {
-        // These numbers must be tuned for your Robot!  Be careful!
-        final double turn = 0.03;                    // how hard to turn toward the target
-        final double forward = 0.26;                    // how hard to drive fwd toward the target
-        final double DESIRED_TARGET_AREA = 13.0;        // Area of the target when the robot reaches the wall
-        final double MAX_DRIVE = 0.7;                   // Simple speed limit so we don't drive too fast
 
-        double tv = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tv").getDouble(0);
-        double tx = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0);
-        double ty = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ty").getDouble(0);
-        double ta = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ta").getDouble(0);
 
-        if (tv < 1.0)
-        {
-          m_LimelightHasValidTarget = false;
-          m_LimelightDriveCommand = 0.0;
-          m_LimelightSteerCommand = 0.0;
-          return;
-        }
+	public void Update_Limelight_Tracking()
+	{
+		// These numbers must be tuned for your Robot!  Be careful!
+		final double STEER_K = 0.03;                    // how hard to turn toward the target
+		final double DRIVE_K = 0.26;                    // how hard to drive fwd toward the target
+		final double DESIRED_TARGET_AREA = 13.0;        // Area of the target when the robot reaches the wall
+		final double MAX_DRIVE = 0.7;                   // Simple speed limit so we don't drive too fast
 
-        m_LimelightHasValidTarget = true;
+		double tv = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tv").getDouble(0);
+		double tx = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0);
+		double ty = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ty").getDouble(0);
+		double ta = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ta").getDouble(0);
 
-        // Start with proportional steering
-        double steer_cmd = tx * forward;
-        m_LimelightSteerCommand = steer_cmd;
+		if (tv < 1.0)
+		{
+			m_LimelightHasValidTarget = false;
+			m_LimelightDriveCommand = 0.0;
+			m_LimelightSteerCommand = 0.0;
+			return;
+		}
 
-        // try to drive forward until the target area reaches our desired area
-        double drive_cmd = (DESIRED_TARGET_AREA - ta) * forward;
+		m_LimelightHasValidTarget = true;
 
-        // don't let the robot drive too fast into the goal
-        if (drive_cmd > MAX_DRIVE)
-        {
-          drive_cmd = MAX_DRIVE;
-        }
-        m_LimelightDriveCommand = drive_cmd;
-  }
+		// Start with proportional steering
+		double steer_cmd = tx * STEER_K;
+		m_LimelightSteerCommand = steer_cmd;
+
+		// try to drive forward until the target area reaches our desired area
+		double drive_cmd = (DESIRED_TARGET_AREA - ta) * DRIVE_K;
+
+		// don't let the robot drive too fast into the goal
+		if (drive_cmd > MAX_DRIVE)
+		{
+			drive_cmd = MAX_DRIVE;
+		}
+		m_LimelightDriveCommand = drive_cmd;
+	}
 }
