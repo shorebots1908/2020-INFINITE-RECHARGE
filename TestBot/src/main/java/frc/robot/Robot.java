f92c470b996a37640ac5aa924670bb2f736621fe
shorebots1908 <60295290+shorebots1908@users.noreply.github.com>
2020-02-01 12:30:21 -0500

    Code Fixes

--- TestBot/src/main/java/frc/robot/Robot.java
+++ TestBot/src/main/java/frc/robot/Robot.java
@@ -7,30 +7,18 @@
 
 package frc.robot;
 
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.interfaces.Gyro;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.interfaces.Gyro;
-/* import edu.wpi.first.wpilibj.drive.DifferentialDrive; */
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.DemandType;
-import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-
-import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.ADXRS450_Gyro;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.SPI;
-import edu.wpi.first.wpilibj.TimedRobot;
-/* import edu.wpi.first.wpilibj.drive.DifferentialDrive; */
-import edu.wpi.first.wpilibj.interfaces.Gyro;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
 /**
@@ -38,7 +26,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * Runs the motors with arcade steering.
  */
 public class Robot extends TimedRobot {
-  Gyro gyro = new ADXRS450_Gyro(SPI.Port.kMXP);
   private final TalonSRX _leftMaster = new TalonSRX(1);
   private final TalonSRX _rightMaster = new TalonSRX(2);
   private final Joystick _gamepad = new Joystick(0);
@@ -46,8 +33,6 @@ public class Robot extends TimedRobot {
   private double m_LimelightDriveCommand = 0.0;
   private double m_LimelightSteerCommand = 0.0;
   Gyro gyro = new ADXRS450_Gyro();
-
-
 
   @Override
 	public void teleopInit(){
@@ -70,14 +55,12 @@ public class Robot extends TimedRobot {
 		gyro.reset();
 		
 		System.out.println("This is Arcade Drive using Arbitrary Feed Forward.");
-		gyro.reset();
 	}
 
   @Override
 	public void teleopPeriodic() {		
 		/* Limelight processing */
 		Update_Limelight_Tracking();
-		SmartDashboard.putNumber("Gyro angle", gyro.getAngle());
 		
 		/* Gamepad processing */
 		double forward = _gamepad.getY();
@@ -95,36 +78,32 @@ public class Robot extends TimedRobot {
 	
 		if (auto)
         {
-          if (m_LimelightHasValidTarget)
-          {
+          if (m_LimelightHasValidTarget){
 				_leftMaster.set(ControlMode.PercentOutput, m_LimelightDriveCommand, DemandType.ArbitraryFeedForward, +m_LimelightSteerCommand);
 				_rightMaster.set(ControlMode.PercentOutput, m_LimelightDriveCommand, DemandType.ArbitraryFeedForward, -m_LimelightSteerCommand);
 	
-          }
-          else
-          {
+          } else {
 				_leftMaster.set(ControlMode.PercentOutput, 0, DemandType.ArbitraryFeedForward, -0);
 				_rightMaster.set(ControlMode.PercentOutput, 0, DemandType.ArbitraryFeedForward, +0);
 
           }
-        }
-        else
-        {
+        } else  {
 			_leftMaster.set(ControlMode.PercentOutput, forward * throttle, DemandType.ArbitraryFeedForward, -turn * throttle);
 			_rightMaster.set(ControlMode.PercentOutput, forward * throttle, DemandType.ArbitraryFeedForward, +turn * throttle);
-		
 		}
 	}
 
 	/** Deadband 5 percent, used on the gamepad */
 	double Deadband(double value) {
 		/* Upper deadband */
-		if (value >= +0.05) 
+		if (value >= +0.05){
 			return value;
+		}			
 		
 		/* Lower deadband */
-		if (value <= -0.05)
+		if (value <= -0.05){
 			return value;
+		}			
 		
 		/* Outside deadband */
 		return 0;
@@ -137,7 +116,7 @@ public class Robot extends TimedRobot {
 		// These numbers must be tuned for your Robot!  Be careful!
 		final double STEER_K = 0.03;                    // how hard to turn toward the target
 		final double DRIVE_K = 0.26;                    // how hard to drive fwd toward the target
-		final double DESIRED_TARGET_AREA = 3.0;        // Area of the target when the robot reaches the wall
+		final double DESIRED_TARGET_AREA = 3.0;         // Area of the target when the robot reaches the wall
 		final double MAX_DRIVE = 0.7;                   // Simple speed limit so we don't drive too fast
 
 		double tv = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tv").getDouble(0);
