a89bf4e42cc513372d0118aeae099bb4155f72f0
Buttwhole <FasterComic1@gmail.com>
2020-02-08 11:35:06 -0500

    Added Color Match

--- TestBot/src/main/java/frc/robot/Robot.java
+++ TestBot/src/main/java/frc/robot/Robot.java
@@ -18,6 +18,9 @@ import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.util.Color;
+
+import com.revrobotics.ColorMatch;
+import com.revrobotics.ColorMatchResult;
 import com.revrobotics.ColorSensorV3;
 
 import com.ctre.phoenix.motorcontrol.NeutralMode;
@@ -43,6 +46,15 @@ public class Robot extends TimedRobot {
   AnalogInput analog = new AnalogInput(0);
   private final I2C.Port i2cPort = I2C.Port.kOnboard;
   private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
+  private final ColorMatch m_colorMatcher = new ColorMatch();
+  
+
+  private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
+  private final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
+  private final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
+  private final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
+
+  
 
 
   // Drive PIDS
@@ -70,6 +82,11 @@ public class Robot extends TimedRobot {
 		_rightMaster.setInverted(false);
 
 		gyro.reset();
+
+		m_colorMatcher.addColorMatch(kBlueTarget);
+		m_colorMatcher.addColorMatch(kGreenTarget);
+		m_colorMatcher.addColorMatch(kRedTarget);
+		m_colorMatcher.addColorMatch(kYellowTarget);
 		
 		System.out.println("This is Arcade Drive using Arbitrary Feed Forward.");
 	}
@@ -90,6 +107,7 @@ public class Robot extends TimedRobot {
 		throttle = (throttle - 1) / 2;
 		boolean servoButton = _gamepad.getRawButton(2);
 		Color detectedColor = m_colorSensor.getColor();
+		
 		double IR = m_colorSensor.getIR();
 		int proximity = m_colorSensor.getProximity();
 	
@@ -101,12 +119,30 @@ public class Robot extends TimedRobot {
 		SmartDashboard.putNumber("turn", turn);
 		SmartDashboard.putNumber("Analog value" ,analog.getValue());
 		SmartDashboard.putNumber("Analog voltage" ,analog.getVoltage());
-		SmartDashboard.putNumber("Red", detectedColor.red);
-    	SmartDashboard.putNumber("Green", detectedColor.green);
-    	SmartDashboard.putNumber("Blue", detectedColor.blue);
     	SmartDashboard.putNumber("IR", IR);
 		SmartDashboard.putNumber("Proximity", proximity);
 		/* Arcade Drive using PercentOutput along with Arbitrary Feed Forward supplied by turn */
+
+		String colorString;
+		ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
+
+		if (match.color == kBlueTarget) {
+		colorString = "Blue";
+		} else if (match.color == kRedTarget) {
+		colorString = "Red";
+		} else if (match.color == kGreenTarget) {
+		colorString = "Green";
+		} else if (match.color == kYellowTarget) {
+		colorString = "Yellow";
+		} else {
+		colorString = "Unknown";
+		}
+
+		SmartDashboard.putNumber("Red", detectedColor.red);
+    	SmartDashboard.putNumber("Green", detectedColor.green);
+		SmartDashboard.putNumber("Blue", detectedColor.blue);
+		SmartDashboard.putNumber("Confidence", match.confidence);
+		SmartDashboard.putString("Detected Color", colorString);
 
 		if (servoButton) {
 			BruhServo.setAngle(75);
